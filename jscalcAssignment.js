56801e07dab808db348eac5e8fb559606ee4e6e4
Niklas Bengtsson <de02beni@gmail.com>
2024-08-18 21:17:39 +0200

    the calc program added in jscalcAssignment

--- jscalcAssignment.js
+++ jscalcAssignment.js
@@ -1,1 +1,34 @@
-console.log("Here start the Calc things");
+function doOperation(inNumber1, inNumber2, operation){
+    if (operation === 1){
+        return inNumber1 + inNumber2;
+    }
+    else if (operation === 2){
+        return inNumber1 - inNumber2;
+    }
+    else if (operation === 3) {
+        return inNumber1 * inNumber2;
+    }
+    else if (operation === 4){
+        return inNumber1 / inNumber2;
+    }
+    else{
+        alert("Invalid operation input, please try again (ctrl + r)");
+        return "";
+    }
+};
+
+console.log("Here start the Calc things, bien venido");
+
+const inNumber1 = window.parseInt(prompt("Type a number"));
+
+const inNumber2 = window.parseInt(prompt("Type one more"));
+
+const operationType = window.parseInt(prompt("Choose type of operation \n 1(addition) \n 2(subtraction) \n 3(multiply) \n 4(divide)")); 
+
+const result = doOperation(inNumber1, inNumber2, operationType);
+
+alert(`The result is: ${result}`);
+    
+//console.log(inNumber);
+
+alert("hasta la vista");
