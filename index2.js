0cddc1957231fe59231849ea39d5c5c1f11d7313
Niklas Bengtsson <de02beni@gmail.com>
2024-08-18 15:10:00 +0200

    Banzai extras

---
+++ index2.js
@@ -0,0 +1,12 @@
+
+//   #### User input article 
+// Alert
+// Prompt
+// Confirm
+// If Conditionals
+// === Comparisons
+// Else, Else if
+// Logical operators
+// Truthy Falsey 
+
+console.log("Banzai");
